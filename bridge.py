e7d2de04cc3ebb6e9c45ad9828eb9acefc0fd19b
Concedo <39025047+LostRuins@users.noreply.github.com>
2023-03-04 16:40:43 +0800

    upgrade to new horde

--- bridge.py
+++ bridge.py
@@ -1,34 +1,36 @@
-### This is a minimal version of the Kobold Horde bridge intended for Unattended use ###
-### Please use it responsibly! ###
+import requests, json, os, time, argparse, urllib3
 
-### - Concedo - ###
-
-import requests, json, time, argparse, urllib3
 import random
+import time
 
 def locallog(obj):
-    #print(obj) # comment to run in full stealth mode and do not output anything at all
+    print(obj) # comment to run in full stealth mode and do not output anything at all
     pass
+
 
 class temp(object):
     def __init__(self):
         random.seed()
-        self.cluster_url = "http://koboldai.net"    # The cluster url       
-        self.kai_url = "http://localhost:5000"      # Where can your bridge reach your KAI instance       
-        self.kai_name = f"KG Auto Instance #{random.randint(-100000000, 100000000)}"  # Keep it random
-        self.api_key = "0000000000"                 # The api_key identifies a unique user in the horde. For unattended use, we use the anon key
+        self.cluster_url = "https://stablehorde.net"
+        self.kai_url = "http://localhost:5000"
+        self.kai_name = f"KG Auto Instance #{random.randint(-100000000, 100000000)}"
+        self.api_key = "0000000000"
         self.priority_usernames = []
 
 cd = temp()
 
+
+
 class kai_bridge():
     def __init__(self):
         self.model = ''
-        self.max_content_length = 1024
+        self.max_context_length = 1024
         self.max_length = 80
         self.current_softprompt = None
         self.softprompts = {}
         self.run = True
+        self.last_retrieved = None
+        self.readytogo = False
 
         #reduce and vary polling intervals to be less obvious
         self.stealth_interval_min = 9
@@ -42,16 +44,39 @@ class kai_bridge():
         self.run = False
     
     def validate_kai(self, kai):
+        if self.readytogo and self.model != '' and (self.last_retrieved is None or time.time() - self.last_retrieved <= 30):
+            return True
+        self.last_retrieved = time.time()
+        locallog("Retrieving settings from KoboldAI Client...")
         try:
             req = requests.get(kai + '/api/latest/model')
             self.model = req.json()["result"]
+            if self.model=="ReadOnly":
+                locallog("Not Ready")
+                return(False)
+            if self.readytogo==False:
+                dummytest = {
+                "prompt": "1",
+                "max_length": 4
+                }
+                dummyresult = requests.post(kai_url + '/api/latest/generate/', json = dummytest)
+                dummyresult = "results" in dummyresult.json()
+                if dummyresult:
+                    locallog("Endpt is ready")
+                    self.readytogo = True
+                else:
+                    locallog("Endpt not ready")
+                    return(False)
+            # Normalize huggingface and local downloaded model names
+            if "/" not in self.model:
+                self.model = self.model.replace('_', '/', 1)
             req = requests.get(kai + '/api/latest/config/max_context_length')
-            self.max_content_length = req.json()["value"]
+            self.max_context_length = req.json()["value"]
             req = requests.get(kai + '/api/latest/config/max_length')
             self.max_length = req.json()["value"]
             if self.model not in self.softprompts:
-                    req = requests.get(kai + '/api/latest/config/soft_prompts_list')
-                    self.softprompts[self.model] = [sp['value'] for sp in req.json()["values"]]
+                req = requests.get(kai + '/api/latest/config/soft_prompts_list')
+                self.softprompts[self.model] = [sp['value'] for sp in req.json()["values"]]
             req = requests.get(kai + '/api/latest/config/soft_prompt')
             self.current_softprompt = req.json()["value"]
         except requests.exceptions.JSONDecodeError:
@@ -63,14 +88,22 @@ class kai_bridge():
         return(True)
 
 
-    def bridge(self, api_key, kai_name, kai_url, cluster, priority_usernames):
+    def bridge(self, 
+        interval, 
+        api_key, 
+        kai_name, 
+        kai_url, 
+        horde_url, 
+        priority_usernames,
+    ):
         current_id = None
         current_payload = None
         return_error = None
         loop_retry = 0
         failed_requests_in_a_row = 0
         interval = 100
-
+        self.BRIDGE_AGENT = f"KoboldAI Bridge:10:https://github.com/db0/KoboldAI-Horde-Bridge"
+        cluster = horde_url
         while self.run:
 
             self.cycles_before_stealth_counter += 1
@@ -79,6 +112,9 @@ class kai_bridge():
             else:
                 interval = random.uniform(self.awake_interval_min, self.awake_interval_max)
 
+            locallog(f"Interval: {interval}")
+
+            headers = {"apikey": api_key}
             if loop_retry > 3 and current_id:
                 locallog(f"Exceeded retry count {loop_retry} for generation id {current_id}. Aborting generation!")
                 current_id = None
@@ -86,6 +122,15 @@ class kai_bridge():
                 current_generation = None
                 return_error = None
                 loop_retry = 0
+                submit_dict = {
+                    "id": current_id,
+                    "state": "faulted",
+                    "generation": "faulted",
+                    "seed": -1,
+                }
+                submit_req = requests.post(cluster + '/api/v2/generate/text/submit', json = submit_dict, headers = headers)
+                if submit_req.status_code == 404:
+                    locallog(f"The generation we were working on got stale. Aborting!")
                 failed_requests_in_a_row += 1
                 if failed_requests_in_a_row > 3:
                     locallog(f"{failed_requests_in_a_row} Requests failed in a row. Crashing bridge!")
@@ -96,20 +141,20 @@ class kai_bridge():
                 locallog(f"Waiting 10 seconds...")
                 time.sleep(10)
                 continue
-            headers = {"apikey": api_key}
             gen_dict = {
                 "name": kai_name,
-                "model": self.model,
+                "models": [self.model],
                 "max_length": self.max_length,
-                "max_content_length": self.max_content_length,
+                "max_context_length": self.max_context_length,
                 "priority_usernames": priority_usernames,
                 "softprompts": self.softprompts[self.model],
+                "bridge_agent": self.BRIDGE_AGENT,
             }
             if current_id:
                 loop_retry += 1
             else:
                 try:
-                    pop_req = requests.post(cluster + '/api/v2/generate/pop', json = gen_dict, headers = headers)
+                    pop_req = requests.post(cluster + '/api/v2/generate/text/pop', json = gen_dict, headers = headers)
                 except (urllib3.exceptions.MaxRetryError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
                     locallog(f"Server {cluster} unavailable during pop. Waiting 10 seconds...")
                     time.sleep(10)
@@ -144,6 +189,7 @@ class kai_bridge():
                 # By default, we don't want to be annoucing the prompt send from the Horde to the terminal
                 current_payload['quiet'] = True
                 requested_softprompt = pop['softprompt']
+            locallog(f"Job received from {cluster} for {current_payload.get('max_length',80)} tokens and {current_payload.get('max_context_length',1024)} max context. Starting generation...")
             if requested_softprompt != self.current_softprompt:
                 req = requests.put(kai_url + '/api/latest/config/soft_prompt/', json = {"value": requested_softprompt})
                 time.sleep(1) # Wait a second to unload the softprompt
@@ -193,7 +239,7 @@ class kai_bridge():
                 }
             while current_id and current_generation:
                 try:
-                    submit_req = requests.post(cluster + '/api/v2/generate/submit', json = submit_dict, headers = headers)
+                    submit_req = requests.post(cluster + '/api/v2/generate/text/submit', json = submit_dict, headers = headers)
                     if submit_req.status_code == 404:
                         locallog(f"The generation we were working on got stale. Aborting!")
                     elif not submit_req.ok:
@@ -206,7 +252,7 @@ class kai_bridge():
                             time.sleep(10)
                             continue
                     else:
-                        locallog(f'Submitted generation with id {current_id} and contributed for {submit_req.json()["reward"]}')
+                        locallog(f'Submitted generation to {cluster} with id {current_id} and contributed for {submit_req.json()["reward"]}')
                         failed_requests_in_a_row = 0
                         self.cycles_before_stealth_counter = 0
                         interval = random.uniform(self.awake_interval_min, self.awake_interval_max)
@@ -220,12 +266,13 @@ class kai_bridge():
                     locallog(f"Server {cluster} unavailable during submit. Waiting 10 seconds...")
                     loop_retry += 1
                     time.sleep(10)
-                    continue
+                    continue            
             time.sleep(interval)
 
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-i', '--interval', action="store", required=False, type=int, default=1, help="The amount of seconds with which to check if there's new prompts to generate")
     arg_parser.add_argument('-a', '--api_key', action="store", required=False, type=str, help="The API key corresponding to the owner of the KAI instance")
     arg_parser.add_argument('-n', '--kai_name', action="store", required=False, type=str, help="The server name. It will be shown to the world and there can be only one.")
     arg_parser.add_argument('-k', '--kai_url', action="store", required=False, type=str, help="The KoboldAI server URL. Where the bridge will get its generations from.")
@@ -234,16 +281,23 @@ if __name__ == "__main__":
     arg_parser.add_argument('--priority_usernames',type=str, action='append', required=False, help="Usernames which get priority use in this server. The owner's username is always in this list.")
     arg_parser.add_argument('-v', '--verbosity', action='count', default=0, help="The default logging level is ERROR or higher. This value increases the amount of logging seen in your screen")
     arg_parser.add_argument('-q', '--quiet', action='count', default=0, help="The default logging level is ERROR or higher. This value decreases the amount of logging seen in your screen")
+    arg_parser.add_argument('--log_file', action='store_true', default=False, help="If specified will dump the log to the specified file")
     args = arg_parser.parse_args()
-    
     api_key = args.api_key if args.api_key else cd.api_key
     kai_name = args.kai_name if args.kai_name else cd.kai_name
     kai_url = args.kai_url if args.kai_url else cd.kai_url
-    cluster = args.cluster_url if args.cluster_url else cd.cluster_url
+    horde_url = args.cluster_url if args.cluster_url else cd.cluster_url
     priority_usernames = args.priority_usernames if args.priority_usernames else cd.priority_usernames
     locallog(f"{kai_name} Instance Started")
     try:
-        kai_bridge().bridge(api_key, kai_name, kai_url, cluster, priority_usernames)
+        kai_bridge().bridge(
+            interval = args.interval, 
+            api_key = api_key, 
+            kai_name= kai_name,
+            kai_url = kai_url, 
+            horde_url = horde_url, 
+            priority_usernames=priority_usernames,
+        )
     except KeyboardInterrupt:
         locallog(f"Keyboard Interrupt Received. Ending Process")
     locallog(f"{kai_name} Instance Stopped")
